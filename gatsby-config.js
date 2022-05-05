c02ab3bacc7019113169eee95734f3f97d8d6ce0
Kennybabs-py <babalolakehinde74@gmail.com>
2022-05-04 18:19:18 -0700

    added image

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [`gatsby-plugin-sass`],
-}
+  plugins: [`gatsby-plugin-sass`, "gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react";
 import Header from "../components/header";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <main>
       <Header pageTitle="Home" />
       <h1>Hello</h1>
+      <StaticImage
+        alt="photo of kenny longing for the future"
+        src="../images/displaypicture.jpg"
+      />
     </main>
   );
 };
